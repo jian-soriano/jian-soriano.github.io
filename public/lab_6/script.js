4036b5552ecff3ba988591bf848c087c4d1cc8bf
Jian Soriano <jsoriano.tdk@gmail.com>
2020-10-14 11:32:18 -0400

    update index.html title/header, first two JS q's

--- public/lab_6/script.js
+++ public/lab_6/script.js
@@ -30,6 +30,31 @@ document.body.addEventListener('submit', async (e) => {
     .then((fromServer) => fromServer.json())
     .then((fromServer) => {
       // You're going to do your lab work in here. Replace this comment.
+
+      //  Get 10 random countries from the returned value list
+      const allCountries = fromServer;
+      /*const countries = range(10);
+      countries.map((country) => {
+        let newCountry = allCountries[Math.floor(Math.random() * (allCountries.length - 1))];
+        console.log(newCountry);
+        while (this.includes(newCountry)) {
+          newCountry = allCountries[Math.floor(Math.random() * (allCountries.length - 1))];
+          console.log(newCountry);
+        }
+        return newCountry;
+      });
+      console.log(allCountries[1]);
+      console.log(countries);*/
+
+      let countries = range(10).map((country) => allCountries[Math.floor(Math.random() * (allCountries.length - 1))]);
+      console.log(countries);
+      
+      //  Sort the ten countries in reverse alphabetical order using .sort() and the sort function provided
+      countries.sort((a, b) => sortFunction(a, b, 'name'));
+      console.log(countries);
+
+      //  Inject an ordered list element with the classname "flex-inner" into your document.
+
       console.log('fromServer', fromServer);
     })
     .catch((err) => console.log(err));
